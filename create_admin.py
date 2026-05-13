0b7a6f4151b2710002dd787b94020aea5a0f30cb
Carlos Bardelorri <carlosbardelotti@gmail.com>
2026-05-13 11:32:09 -0300

    Nova senha

--- create_admin.py
+++ create_admin.py
@@ -1,21 +1,27 @@
 import os
 import django
 
+# Define as configurações do Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django.setup()
 
-from apps.usuarios.models import Usuario # Verifique se o caminho do seu modelo está correto
+# Importa o seu modelo de usuário customizado
+from apps.usuarios.models import Usuario 
 
 def create_superuser():
-    username = 'admin'
-    email = 'admin@email.com'
-    password = '227567c#' # Mude isso depois!
+    username = 'eduardo'
+    email = 'eduardo@email.com'
+    password = 'duda12345678' # Altere para a senha que desejar
 
     if not Usuario.objects.filter(username=username).exists():
-        Usuario.objects.create_superuser(username, email, password)
-        print("Superusuário criado com sucesso!")
+        Usuario.objects.create_superuser(
+            username=username, 
+            email=email, 
+            password=password
+        )
+        print(f"Superusuário '{username}' criado com sucesso!")
     else:
-        print("Superusuário já existe.")
+        print(f"Superusuário '{username}' já existe.")
 
 if __name__ == '__main__':
     create_superuser()
